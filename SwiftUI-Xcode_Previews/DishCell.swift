7909fcbd652bae3230637528f8d377734fd320fa
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-27 17:05:14 +0530

    first commit

---
+++ SwiftUI-Xcode_Previews/DishCell.swift
@@ -0,0 +1,20 @@
+//
+//  DishCell.swift
+//  SwiftUI-Xcode_Previews
+//
+//  Created by Arpit Dixit on 27/06/21.
+//
+
+import SwiftUI
+
+struct DishCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DishCell_Previews: PreviewProvider {
+    static var previews: some View {
+        DishCell()
+    }
+}
---
+++ SwiftUI-Xcode_Previews/New Group/Dish.swift
@@ -0,0 +1,8 @@
+//
+//  Dish.swift
+//  SwiftUI-Xcode_Previews
+//
+//  Created by Arpit Dixit on 27/06/21.
+//
+
+import Foundation
