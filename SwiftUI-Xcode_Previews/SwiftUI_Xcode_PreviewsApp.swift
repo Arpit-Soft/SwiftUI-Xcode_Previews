b289512ca0662841af631aff2994f8c4b27f8d58
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-27 15:07:52 +0530

    Initial Commit

---
+++ SwiftUI-Xcode_Previews/SwiftUI_Xcode_PreviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Xcode_PreviewsApp.swift
+//  SwiftUI-Xcode_Previews
+//
+//  Created by Arpit Dixit on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Xcode_PreviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
